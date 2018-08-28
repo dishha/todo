bdad380837308e238425ecd35ceeec6594147559
dishamendiratta@gmail.com <dishamendiratta@gmail.com>
2018-08-28 12:04:32 +0530

    added files

--- todo/settings.py
+++ todo/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 #static files
 STATIC_PATH = os.path.join(BASE_DIR,'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
@@ -88,7 +88,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
